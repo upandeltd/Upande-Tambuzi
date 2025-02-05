aab94618fdca9319a8d40ec7b113b4c2bea758e5
Mica2000 <davidatmaina@gmail.com>
2025-02-05 13:39:38 +0300

    created dispatch form

---
+++ upande_tambuzi/upande_tambuzi/doctype/dispatch_form/test_dispatch_form.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Upande Limited and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDispatchForm(FrappeTestCase):
+	pass
